b21bb504b447c50d4714baab6a1c0cc1e5e1bbd5
Nathan Thurmond <NathanThurmond@yahoo.com>
2016-04-03 02:23:41 +0000

    Developed main page layout. Added fonts and images. Set styling and some basic structure for site.

--- index.php
+++ index.php
@@ -1,43 +1,30 @@
-<?php
-
-require_once('config/config.php');
-
-//echo 'Current PHP version: ' . phpversion();
-//print_r(get_loaded_extensions());
-//echo extension_loaded("mongo") ? "loaded\n" : "not loaded\n";
-
-//$connection = new Mongo("mongodb://" . MONGO_USER . ":" . MONGO_PASS . "@" . MONGO_HOST);
-$connection = new Mongo('mongodb://127.0.0.1:27017');
-
-$db = $connection->selectDB(MONGO_DB);
-//$db->setReadPreference(MongoClient::RP_NEAREST, array());
-$collection = $db->selectCollection(MONGO_COLL);
-
-$command = '{"walk":"6"}';
-$returnKeys = '{"mechName":1}';
-
-$cursor = $collection->find(json_decode($command, true), json_decode($returnKeys, true));
-
-foreach($cursor as $doc) {
-    var_dump($doc);
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <title>Mech Markup</title>
+        <link rel="icon" href="images/mechIcon.png">
+        <link rel="stylesheet" href="css/mainPage.css">
+        
         <script src="js/jquery-1.12.2.min.js"></script>
     </head>
     <body>
-        <h1>HELLO WORLD!</h1>
+        <div id='menuBar'>
+            <div class='menuBarTabs' id='allMechs'>
+                <div class='menuBarTabsContainer'>
+                    <div>
+                        <p>All Mechs</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
     
     <script>
         $(document).ready(function() {
             
+            $.getJSON('phpScripts/getMechListings.php', function(allMechs) {
+                console.log(allMechs);    
+            });
         })
     </script>
 </html>
